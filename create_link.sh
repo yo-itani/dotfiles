7dd90e2618a84ac1866a6235d814c29fe9761c30
yo itani <itani@bitstar.jp>
2014-12-03 09:35:50 +0900

    シンボリックリンク作成スクリプト修正 python.txt追加

--- create_link.sh
+++ create_link.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.bashrc ~/
-ln -s ~/dotfiles/.vim ~/
-ln -s ~/dotfiles/.vimrc ~/
+SCRIPTDIR=$(dirname $0)
+create_symlink() {
+    ABSPATH="$(cd $(dirname $1) && pwd)"
+    SRC="$ABSPATH/$(basename $1)"
+    ABSPATH="$(cd $(dirname $2) && pwd)"
+    DST="$ABSPATH/$(basename $2)"
+    if [ -L "$DST" ]; then
+        #echo $(ls -la `dirname $2` | grep "`basename $2`")
+        while :
+        do
+            echo -n "$DST symlink exists. overwrite ? [Y/n]: "
+            read YESNO
+            if [ "$YESNO" = "Y" ]; then
+                rm "$DST"
+                ln -s -v "$SRC" "$DST"
+                break
+            elif [ "$YESNO" = "n" ]; then
+                break
+            fi
+        done
+    elif [ -e "$DST" ]; then
+        #diff -u $1 $2
+        while :
+        do
+            echo "$2 exists. overwrite ? [Y/n]"
+            read YESNO
+            if [ "$YESNO" = "Y" ]; then
+                BACKUPFILE=$DST.$(date +%Y%m%d)
+                cp $DST $BACKUPFILE
+                echo "backup file($BACKUPFILE) created"
+                rm "$DST"
+                ln -s -v "$SRC" "$DST"
+                break
+            elif [ "$YESNO" = "n" ]; then
+                break
+            fi
+        done
+    else
+        ln -s -v "$SRC" "$DST"
+    fi
+}
+
+SCRIPTDIR="$(cd $(dirname $0) && pwd)"
+create_symlink "$SCRIPTDIR/.bashrc" ~/.bashrc
+create_symlink "$SCRIPTDIR/.vim" ~/.vim
+create_symlink "$SCRIPTDIR/.vimrc" ~/.vimrc
